c7a98b756861e1b2e6ee8dfb6a4725e2e8142dc8
jehuamanr@uni.pe <jehuamanr@uni.pe>
2022-12-29 17:37:51 -0500

    Poryecto23

--- utils/plots.py
+++ utils/plots.py
@@ -99,8 +99,8 @@ class Annotator:
                 # self.draw.text((box[0], box[1]), label, fill=txt_color, font=self.font, anchor='ls')  # for PIL>8.0
                 self.draw.text((box[0], box[1] - h if outside else box[1]), label, fill=txt_color, font=self.font)
         else:  # cv2
-            img = detector.findPose(self.im,draw=True)
-            lmList, bboxInfo = detector.findPosition(img,draw=True,bboxWithHands=False)
+            self.im = detector.findPose(self.im,draw=True)
+            lmList, bboxInfo = detector.findPosition(self.im,draw=True,bboxWithHands=False)
             
             p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
             # print("--------------------")
